613a7a1c489973178496e0183939c0a4d27c84e8
龙国飞 <long.gf@belle.com.cn>
2019-01-24 17:35:05 +0800

    init

--- demo-actviemq/demo-activemq-springboot/src/main/java/com/lgfei/demo/activemq/springboot/AppBootstrap.java
+++ demo-actviemq/demo-activemq-springboot/src/main/java/com/lgfei/demo/activemq/springboot/AppBootstrap.java
@@ -7,10 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
 public class AppBootstrap
 {
