3af7bc482676ca52b3983903eb7e5a7556463240
long.gf <long.gf@topsports.com.cn>
2025-10-15 10:52:16 +0800

    init

---
+++ demo-ksqldb/src/main/java/com/lgfei/demo/ksqldb/DemoKsqldbApplication.java
@@ -0,0 +1,13 @@
+package com.lgfei.demo.ksqldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKsqldbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKsqldbApplication.class, args);
+	}
+
+}
---
+++ demo-ksqldb/src/test/java/com/lgfei/demo/ksqldb/DemoKsqldbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgfei.demo.ksqldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKsqldbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
